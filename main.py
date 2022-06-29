a2cc98f2be0833a90d4a227b46c12924f7bc92d0
IVAN <ivashkeevich.inc@gmail.com>
2022-06-30 00:48:44 +0300

    #1 delete spaces

--- main.py
+++ main.py
@@ -3,13 +3,11 @@
 import random
 
 alph = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM~!@#$%&*()_1234567890'
-
 number = int( input('Какое количество паролей необходимо? '))
 lenght = int( input('Какая нужна длина пароля? '))
 
 for x in range( number ):
     password = ''
-
     for i in range (lenght):
         password+=random.choice( alph )
     print(password)
